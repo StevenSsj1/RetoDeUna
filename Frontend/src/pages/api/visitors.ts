4c1eded799851070889386e27f99f4a75bc41281
JossueSM <jsimancasm@est.ups.edu.ec>
2025-03-09 16:08:55 -0500

    Update 1

---
+++ Frontend/src/pages/api/visitors.ts
@@ -0,0 +1,36 @@
+// src/pages/api/transactions/[userId].ts
+import type { APIRoute } from "astro"
+
+export const GET: APIRoute = async ({ params, request }) => {
+  try {
+    const userId = params.userId
+    // Tomamos el query param "format"
+    const url = new URL(request.url)
+    const formatParam = url.searchParams.get("format") || "month"
+
+    // Llamamos a nuestro backend de FastAPI
+    const response = await fetch(
+      `http://localhost:8000/hackemate/user_transactions/${8}?format=date`
+    )
+
+    if (!response.ok) {
+      return new Response("Error llamando al backend", { status: 500 })
+    }
+
+    const data = await response.json()
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: String(error) }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  }
+}
