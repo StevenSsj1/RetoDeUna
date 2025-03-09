6b79ca2e2c75485bc67304ea74a12d8b845d9179
Kevin Castillo <96590435+ronnyR4815@users.noreply.github.com>
2025-03-08 22:14:46 -0500

    Middleware added
    Middleware added

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from .routers import user  # Importamos el router de usuarios
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="HackeMate DeUna", version="1.0.0")
 
-# Incluir routers (agrupaciones de endpoints)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],    # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],    # Permite todos los headers
+)
+
 app.include_router(user.router)
